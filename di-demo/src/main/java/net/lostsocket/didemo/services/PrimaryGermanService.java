75e4413364f6e942f884220384fe27348bc8e65d
4nc3str4l <cmuri_10@hotmail.com>
2018-07-04 21:57:39 +0200

    Default profile

--- di-demo/src/main/java/net/lostsocket/didemo/services/PrimaryGermanService.java
+++ di-demo/src/main/java/net/lostsocket/didemo/services/PrimaryGermanService.java
@@ -6,13 +6,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile({"en", "default"})
-public class GreetingServiceImpl implements GreetingService {
-
-    public static final String HELLO_GURUS = "Hello Gurus!!!!";
+@Profile("de")
+public class PrimaryGermanService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return HELLO_GURUS;
+        return "Primärer Gru";
     }
 }
