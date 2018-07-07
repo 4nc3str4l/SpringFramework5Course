de89a31082f631fe2807e4971c1095ee21cfa37a
4nc3str4l <cmuri_10@hotmail.com>
2018-07-07 17:02:40 +0200

    Add ingredients

--- recipe-app/src/main/java/net/lostsocket/recipeapp/domain/Recipe.java
+++ recipe-app/src/main/java/net/lostsocket/recipeapp/domain/Recipe.java
@@ -3,6 +3,7 @@ package net.lostsocket.recipeapp.domain;
 import org.hibernate.annotations.Cascade;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -18,6 +19,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -103,5 +107,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
