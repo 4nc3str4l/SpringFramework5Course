24f346c2c1dd85129f91c8081988ecf63a9e3c6a
4nc3str4l <cmuri_10@hotmail.com>
2018-07-07 18:27:47 +0200

    refactoring with project lombok

--- recipe-app/src/main/java/net/lostsocket/recipeapp/services/RecipeServiceImpl.java
+++ recipe-app/src/main/java/net/lostsocket/recipeapp/services/RecipeServiceImpl.java
@@ -1,5 +1,6 @@
 package net.lostsocket.recipeapp.services;
 
+import lombok.extern.slf4j.Slf4j;
 import net.lostsocket.recipeapp.domain.Recipe;
 import net.lostsocket.recipeapp.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
         Set<Recipe> recipesSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipesSet::add);
         return recipesSet;
