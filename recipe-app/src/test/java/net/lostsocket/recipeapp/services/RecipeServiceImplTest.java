05650b18ef5cac86d118edd3010c5014d802c09d
4nc3str4l <cmuri_10@hotmail.com>
2018-08-20 16:30:12 +0200

    TDD on get recipe by id.

--- recipe-app/src/test/java/net/lostsocket/recipeapp/services/RecipeServiceImplTest.java
+++ recipe-app/src/test/java/net/lostsocket/recipeapp/services/RecipeServiceImplTest.java
@@ -7,13 +7,14 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import javax.swing.text.html.Option;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 public class RecipeServiceImplTest {
 
@@ -29,7 +30,22 @@ public class RecipeServiceImplTest {
     }
 
     @Test
-    public void getRecipes() {
+    public void getRecipeByIdTest() throws Exception{
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull("Null recipe received", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
+    public void getRecipesTest() {
 
         Recipe recipe = new Recipe();
         HashSet receipesData = new HashSet();
